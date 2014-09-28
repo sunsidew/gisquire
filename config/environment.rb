c8139637ef3aaa365deb58ba0dc570dff9b3d67a
sunsidew <danielay@naver.com>
2014-09-28 22:44:17 +0900

    token exposure closed by regeneration

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# token regenerated (this is invalid token)
 ENV['OCTOKIT_TOKEN'] ||= "bffcbde1c5a883e4d1468105f67ef70fc9b94953"
 ENV['OCTOKIT_TARGET_REPO'] ||= "sunsidew/gisquire"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
+# token regenerated (this is invalid token)
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "663435676220-jg0p5lpbpo183rqsn4nnb4hl0ajr0efo.apps.googleusercontent.com", "4wvFGtlGCp_dyeK1ncX6DbAz"
 end
